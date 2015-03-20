b671407480628e42158addd1aaf3bfd486f65e12
wwqk4444 <wwqk4444@163.com>
2015-03-20 14:30:37 +0800

    系统消息已经能做到群发 添加 与查看

--- console/lib/YqSystemMessage.php
+++ console/lib/YqSystemMessage.php
@@ -1,8 +1,98 @@
 <?php
 require_once 'YqMessage.php';
 class YqSystemMessage extends YqMessage {
-	function addSystemMessage($toall, $message_receiverId, $message_type, $message_title, $message_labels, $message_topicID, $message_topicTitle, $message_detail) {
-		return $this->addMessagev2 ( 'system', $message_receiverId, $message_type, $message_title, $message_labels, $message_topicID, $message_topicTitle, $message_detail );
+	function addSystemMessage($toall = 0, $message_receiverId = '', $message_type, $message_title, $message_labels, $message_detail) {
+		if ($toall == 1) {
+			$cus = $this->db->user->find ();
+			$con = 0;
+			while ( $cus->hasNext () ) {
+				$doc = $cus->getNext ();
+				if ($doc ['user_name'] != 'system') {
+					if ($this->addMessagev2 ( 'system', $doc ['user_name'], $message_type, $message_title, $message_labels, '', '', $message_detail )) {
+						$con ++;
+					}
+				}
+			}
+			return $con;
+		}
+		return $this->addMessagev2 ( 'system', $message_receiverId, $message_type, $message_title, $message_labels, '', '', $message_detail );
+	}
+	function getSystemMessage($way, $type = '', $starttime = 0, $endtime) {
+		try {
+			$endtime = ( int ) $endtime;
+			$starttime = ( int ) $starttime;
+			if ($way == 'recive') {
+				if ($type == '') {
+					$result = $this->db->message->find ( array (
+							'message_receiverId' => 'system',
+							'message_postTime' => array (
+									'$lt' => $endtime,
+									'$gte' => $starttime 
+							) 
+					) )->sort ( array (
+							'message_postTime' => - 1 
+					) );
+				} else {
+					$result = $this->db->message->find ( array (
+							'message_receiverId' => 'system',
+							'message_postTime' => array (
+									'$lt' => $endtime,
+									'$gte' => $starttime 
+							),
+							
+							'message_type' => $type 
+					) )->sort ( array (
+							'message_postTime' => - 1 
+					) );
+				}
+			} else if ($way == 'send') {
+				if ($type == '') {
+					$result = $this->db->message->find ( array (
+							'message_senderId' => 'system',
+							'message_postTime' => array (
+									'$lt' => $endtime,
+									'$gte' => $starttime 
+							) 
+					) )->sort ( array (
+							'message_postTime' => - 1 
+					) );
+				} else {
+					$result = $this->db->message->find ( array (
+							'message_senderId' => 'system',
+							'message_postTime' => array (
+									'$lt' => $endtime,
+									'$gte' => $starttime 
+							),
+							'message_type' => $type 
+					) )->sort ( array (
+							'message_postTime' => - 1 
+					) );
+				}
+			}
+			$count = 0;
+			$res = array ();
+			foreach ( $result as $key => $value ) {
+				$user = $this->db->user->findOne ( array (
+						'user_name' => $value ['message_senderId'] 
+				) );
+				$value ['sender_nickname'] = $user ['user_nickname'];
+				$value ['sender_pic'] = $user ['user_pic'];
+				array_push ( $res, $value );
+				$count ++;
+			}
+			return $res;
+		} catch ( Exception $e ) {
+			return - 1;
+		}
+	}
+	function getSystemMessagePages($arr, $limit) {
+		$res = [ ];
+		for($i = 0; $i < count ( $arr ); $i ++) {
+			if ($i % $limit == 0) {
+				$res [] = $i;
+			}
+		}
+		return $res;
 	}
 }
 
--- console/lib/YqSystemMessageView.php
+++ console/lib/YqSystemMessageView.php
@@ -3,15 +3,14 @@ require_once 'YqSystemMessage.php';
 class YqSystemMessageView extends YqSystemMessage {
 	function showMessagetype($name) {
 		echo '<select name="' . $name . '">';
-	    echo '<option value="userMessage">userMessage</option>';
-	    echo '<option value="webview">webview</option>';
-	    echo '<option value="systemNotice">systemNotice</option>';
-	    echo '<option value="newReply">newReply</option>';
-	    echo '<option value="invitation">invitation</option>';
-	    echo '</select>';
+		echo '<option value="userMessage">userMessage</option>';
+		echo '<option value="webview">webview</option>';
+		echo '<option value="systemNotice">systemNotice</option>';
+		echo '<option value="invitation">invitation</option>';
+		echo '</select>';
 	}
 	function addSystemMessage_form() {
-		echo '<div><form method="post" action="?action=addsystemtopic">';
+		echo '<div><form method="post" action="?action=addSystemMessage">';
 		echo '<div class="form-group"><h2>请输入消息标题</h2>';
 		echo '<textarea class="form-control" rows="3" cols="80" name="title"></textarea></div>';
 		echo '<div class="form-group"><h2>请输入消息内容</h2>';
@@ -21,12 +20,47 @@ class YqSystemMessageView extends YqSystemMessage {
 		echo '<div class="form-group"><h2>请输入标签</h2>';
 		echo '<input class="form-control" type="text"  name="labels" /></div>';
 		echo '<div class="form-group"><h2>请选择消息类型</h2>';
-		$this->showMessagetype('type');
+		$this->showMessagetype ( 'type' );
 		echo '</div>';
 		echo '<div class="form-group"><h2>是否群发</h2>';
-		echo '<input class="form-control" type="checkbox"  name="forall" value="0"/></div>';
+		echo '<input class="form-control" type="checkbox"  name="forall" /></div>';
 		echo '<div class="form-group"><input type="submit" value="提交" /></div>';
 		echo '</form></div>';
+	}
+	function showMessage_table($arr, $start, $limit) {
+		echo '<div class="table-responsive"><table class="table table-striped">';
+		echo '<thead><tr><th>发件人</th><th>收件人</th><th>标题</th><th>发送时间</th><th>类型</th><th>未读</th><th>操作1</th></tr></thead>';
+		for($i = $start; $i <= $start + $limit - 1 && $i < count ( $arr ); $i ++) {
+			echo '<tr>';
+			echo '<td>' . $arr [$i] ['message_senderId'] . '</td>';
+			echo '<td>' . $arr [$i] ['message_receiverId'] . '</td>';
+			echo '<td>' . $arr [$i] ['message_title'] . '</td>';
+			echo '<td>' . date ( "Y-m-d h:m:s", $arr [$i] ['message_postTime'] ) . '</td>';
+			echo '<td>' . $arr [$i] ['message_type'] . '</td>';
+			echo '<td>' . $arr [$i] ['message_life'] . '</td>';
+			echo '<td><a href="?action=detail&mindex=' . $i . '&messageid=' . ($arr [$i] ['_id']->{'$id'}) . '">详细</a></td>';
+			echo '</tr>';
+		}
+		echo '</table></div>';
+	}
+	function showMessagePages_div($arr, $action) {
+		echo '<div>';
+		for($i = 0; $i < count ( $arr ); $i ++) {
+			echo '<a href="?action=' . $action . '&page=' . $i . '&limit=30">' . ($i + 1) . '</a>  ';
+		}
+		echo '</div>';
+	}
+	function showMessageDetail($arr, $mindex) {
+		echo '<div class="table-responsive"><table class="table table-striped">';
+		echo '<tr><td>发件人</td><td>' . $arr [$mindex] ['message_senderId'] . '</td></tr>';
+		echo '<tr><td>收件人</td><td>' . $arr [$mindex] ['message_receiverId'] . '</td></tr>';
+		echo '<tr><td>主题</td><td>' . $arr [$mindex] ['message_title'] . '</td></tr>';
+		if (isset ( $arr [$mindex] ['message_detail'] )) {
+			echo '<tr><td>内容</td><td>' . $arr [$mindex] ['message_detail'] . '</td></tr>';
+		}
+		echo '<tr><td>时间</td><td>' . $arr [$mindex] ['message_postTime'] . '</td></tr>';
+		echo '<tr><td>类型</td><td>' . $arr [$mindex] ['message_type'] . '</td></tr>';
+		echo '</table></div>';
 	}
 }
 ?>
