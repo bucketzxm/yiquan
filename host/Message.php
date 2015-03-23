36d8dd133f1440b96171fb744fb589e7726ec5ce
Zhu Lun <lonblues@gmail.com>
2015-03-23 13:16:20 +0800

    -Add the PushMessage API and code for apple APNS and Getui Android (unfinished);

--- host/Message.php
+++ host/Message.php
@@ -1,5 +1,18 @@
 <?php
 require_once 'YqBase.php';
+require_once 'getui/IGt.Push.php');
+require_once 'getui/igetui/IGt.AppMessage.php');
+require_once 'getui/igetui/template/IGt.BaseTemplate.php');
+    
+define('APPKEY','Fswp6NteiyAgshqIjY4UTA');
+define('APPID','ksCvaUMV9D7rhBA1vMydXA');
+define('MASTERSECRET','Ava39nShg88cpX8DydftJ3');
+define('CID','');
+
+define('DEVICETOKEN','');
+define('HOST','http://sdk.open.api.igexin.com/apiex.htm');
+
+    
 class Message extends YqBase {
 	private $collection;
 	// static $conn; // 连接
@@ -82,6 +95,16 @@ class Message extends YqBase {
 				);
 			if ( ($cursor == NULL) || ($message_type != 'newReply')) {
 				$result = $this->db->message->insert ( $data );
+                
+                $cursor = $this->db->getuiClientID->findOne(array ('user_name' => $message_receiverId));
+                if ($cursor != null) {
+                    $platform = $cursor ['platform'];
+                    $clientID = $cursor ['getui_clientID'];
+                    if ($platform == 'iOS'){
+                        $this->pushiOSMessage($clientID);
+                    }
+                }
+                
 				return 1;
 			}
 			else
@@ -91,6 +114,76 @@ class Message extends YqBase {
 			return - 1;
 		}
 	}
+    
+    
+    //发送新消息通知给相应的人
+    function pushiOSMessage（$clientID）{
+        
+        $ctx = stream_context_create();
+        stream_context_set_option($ctx,'ssl','local_cert','yqAPNS.pem');
+        stream_context_set_option($ctx,'ssl','passphrase','2015oneto');
+        $fp = stream_socket_client('ssl://gateway.push.apple.com:2195',$err,$errstr,60,STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+        
+        if (!$fp)
+            exit("Failed to connect: $err $errstr" . PHP_EOL);
+        
+        ECHO 'Connected to APNS' . PHP_EOL;
+        
+        $body['aps'] = array (
+            'alert' => '今天天气真好',
+            'sound' => 'default'
+        );
+        
+        $payload = json_encode($body);
+        
+        // Build the binary notification
+        $msg = chr(0) . pack('n', 32) . pack('H*', $clientID) . pack('n', strlen($payload)) . $payload;
+        
+        // Send it to the server
+        $result = fwrite($fp, $msg, strlen($msg));
+        
+        if (!$result)
+            echo 'Message not delivered' . PHP_EOL;
+        else
+            echo 'Message successfully delivered' . PHP_EOL;
+        
+        // Close the connection to the server
+        fclose($fp);
+        
+        /*
+        $igt = new IGeTui("",APPKEY,MASTERSECRET);
+        
+        $template =  new IGtTransmissionTemplate();
+        //应用appid
+        $template->set_appId(APPID);
+        //应用appkey
+        $template->set_appkey(APPKEY);
+        //透传消息类型
+        $template->set_transmissionType(1);
+        //透传内容
+        $template->set_transmissionContent("新的消息");
+        $template->set_pushInfo("actionLocKey","0","message",
+           "sound","payload","locKey","locArgs","launchImage");
+        
+        $begin = "2015-03-06 13:18:00";
+        $end = "2015-03-06 13:24:00";
+        $template ->set_duration($begin,$end);
+        
+        $message = new IGtSingleMessage();
+        $message->set_isOffline(true);//是否离线
+        $message->set_offlineExpireTime(3600*12*1000);//离线时间
+        $message->set_data($template);//设置推送消息类型
+        //接收方
+        $target = new IGtTarget();
+        $target->set_appId(APPID);
+        $target->set_clientId($clientID);
+        
+        $rep = $igt->pushMessageToSingle($message,$target);
+        var_dump($rep);
+         */
+        
+    }
+    
 	
 	// 查询用户收到的
 	function queryMessageByName($message_receiverId,$time) {
