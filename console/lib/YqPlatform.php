c1d994cf4b93f86171c495431f3bde615bfd10d6
Zhu Lun <lonblues@gmail.com>
2015-04-03 01:28:55 +0800

    - 增加了topic_labels 长话题 标签 数据维护；

--- console/lib/YqPlatform.php
+++ console/lib/YqPlatform.php
@@ -161,6 +161,9 @@ class YqPlatform extends YqBase {
 		
 		while ( $cus->hasNext () ) {
 			$doc = $cus->getNext ();
+            if (count($doc['topic_labels']) == 2 && $doc['topic_detail'] != ''){
+                array_push ($doc['topic_labels'],"长话题");
+            }
 			if (! isset ( $doc ['topic_followNames'] )) {
 				$doc ['topic_followNames'] = [ ];
 			} else {
