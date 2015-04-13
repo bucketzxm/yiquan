e8a61fe85569fe493c5a55f4be21e8e5f3295154
Zhu Lun <lonblues@gmail.com>
2015-04-13 19:55:30 +0800

    - Add query by all group function;

--- host/Topic.php
+++ host/Topic.php
@@ -470,6 +470,58 @@ class Topic extends YqBase {
 			return - 1;
 		}
 	}
+    
+    function queryTopicByAllGroup ($user_name, $topic_time){
+        if ($this->yiquan_version == 0) {
+            return - 2;
+        }
+        
+        if ($this->checkToken () == 0) {
+            return - 3;
+        }
+        $this->logCallMethod ( $this->getCurrentUsername (), __METHOD__ );
+        $time_int = ( int ) $topic_time;
+        
+        $user = $this->db->user->findOne (array ( 'user_name' => $user_name));
+        $groups = $user['user_groups'];
+        
+        $userclass = new User ();
+        $firstList = $userclass->listFirstFriendsByName ($user_name);
+        $first_array = explode (',',$firstList);
+        
+        try {
+            $cursor = $this->db->topic->find (array (
+                                           '$or' => array (
+                                                           array (
+                                                                  'topic_group' => array ('$in' => $groups),
+                                                                  'topic_postTime' => array ('$lt' => $time_int)
+                                                        ),
+                                                           array (
+                                                                'topic_ownerName' => array ('$in' => $first_array),
+                                                                  'topic_postTime' => array ('$lt' => $time_int),
+                                                                  'topic_group' => 'second'
+                                                           )
+                                                  )
+                                           )
+                                           ))->sort ( 'topic_postTime' => -1)->limit (30);
+            
+            $res = array ();
+            foreach ( $cursor as $key => $value ) {
+                $user_nickname = $this->db->user->findOne ( array (
+                                                                   'user_name' => $value ['topic_ownerName']
+                                                                   ), array (
+                                                                             'user_nickname' => 1
+                                                                             ) );
+                $value ['user_nickname'] = $user_nickname ['user_nickname'];
+                array_push ( $res, $value );
+            }
+            return json_encode ($res);
+        }catch (Exception $e){
+            return $e;
+        }
+        
+        
+    }
 	
     // 按照组别查询
     function queryTopicByGroup($group_user, $group_id,$topic_time) {
@@ -496,6 +548,10 @@ class Topic extends YqBase {
                 $secondList = $user->listAllFriendsByName ($group_user);
                 $secondListWithMe = $group_user . ',' . $secondList;
                 $res = $this->queryTopicByName ($secondListWithMe, 'second', '1', $topic_time);
+                return $res;
+                
+            }else if ($group_id == 'all'){
+                $res = $this->queryTopicByAllGroup ($group_user, $topic_time);
                 return $res;
                 
             }else{
