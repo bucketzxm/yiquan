c9d34b30c2166915998d4a3af451aed6ed5730de
Zhu Lun <lonblues@gmail.com>
2015-04-14 15:49:42 +0800

    - Bug fix;

--- host/Group.php
+++ host/Group.php
@@ -410,12 +410,13 @@ class Group extends YqBase {
         try {
             if  (in_array ($group_member,$group['group_memberList'])){
                 $where = array ('_id' => new MongoId ($group_id));
-                $param = array ('\$pull' => array ( 'group_memberList' => $group_member));
-                $this->db->group->update ($where, $param);
+                $param = array ("\$pull" => array ( 'group_memberList' => $group_member));
+                
+                $result = $this->db->group->update ($where, $param);
             
                 $where1 = array ('user_name' => $group_member);
-                $param1 = array ('\$pull' => array ( 'user_groups' => new MongoId ($group_id)));
-                $this->db->group->update ($where1,$param1);
+                $param1 = array ("\$pull" => array ( 'user_groups' => new MongoId ($group_id)));
+                $result1 = $this->db->user->update ($where1,$param1);
                 
                 return 1;
             }
