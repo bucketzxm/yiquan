21e0560920255cf1abd2a5f4d7a7bce9d56f3afc
Zhu Lun <lonblues@gmail.com>
2015-05-01 13:05:48 +0800

    - debugging;

--- host/Prosource.php
+++ host/Prosource.php
@@ -89,7 +89,7 @@
 
 					//$timeStamp = ;
 					echo "<h2>" . $item->title . "</h2>";
-					echo "<h2>" . $item->link . "</h2>";
+					echo "<h2>" . $titleLen . "</h2>";
 					echo "<h2>" . $postTime. "</h2>";
 					//echo "<p>" . $item->description . "</p>";
 				}
