bf025d215cfa16fb0600bc5b41793c28fa73004e
Zhu Lun <lonblues@gmail.com>
2015-05-02 17:01:51 +0800

    - Bug fix;

--- host/Proseed.php
+++ host/Proseed.php
@@ -66,7 +66,13 @@ class Proseed extends YqBase {
 		$seeds = array ();
 
 		//获得我圈子里赞过的话题
-		$agreedSeeds = $this->db->Proworth>find (array ('like_user' => array ('$in' => $myPros),'like_time' => array ('$gt' => time()-86400*2)),array ('_id' => 1));
+		$agreedSeeds = $this->db->Proworth>find (
+			array (
+				'like_user' => array ('$in' => $myPros),
+				'like_time' => array ('$gt' => time()-86400*2)
+				),
+			array ('_id' => 1)
+			);
 		foreach ($agreedSeeds as $key => $value) {
 			if (in_array ((string)$value['_id'],$seeds)) {
 				# code...
@@ -78,7 +84,12 @@ class Proseed extends YqBase {
 		$user = $this->db->Prouser->findOne (array ('_id' => new MongoId ($user_id)));
 		//获得我关注的媒体的人
 		foreach ($user['user_favorableSource'] as $source) {
-			$sourceSeeds = $this->db->Proseed->find (array ('seed_sourceID' => $source, 'seed_time' => array ('$gt' => time()-86400*1)),array ('_id'=> 1));
+			$sourceSeeds = $this->db->Proseed->find (
+				array (
+					'seed_sourceID' => $source, 
+					'seed_time' => array ('$gt' => time()-86400*1),
+				array ('_id'=> 1)
+				);
 			foreach ($sourceSeeds as $key => $value) {
 				
 				if (in_array ((string)$value['_id'],$seeds)) {
@@ -104,7 +115,9 @@ class Proseed extends YqBase {
 
 		$seeds = array ();
 		foreach ($res as $key => $value) {
-			$selectedSeed = $this->db->Proseed->find(array ('_id'=> new MongoId($key)));
+			$selectedSeed = $this->db->Proseed->find(
+				array ('_id'=> new MongoId($key))
+				);
 			array_push ($seeds,$selectedSeed);
 		}
 		return json_encode($seeds);
@@ -133,7 +146,12 @@ class Proseed extends YqBase {
 			$seed = $this->db->Proseed->findOne (array ('_id' => new MongoId($seed_id)));
 
 			//找到我圈子里的人对这个话题的赞
-			$agrees = $this->db->Proworth->find (array ('like_seed'=>$seed_id,'like_user'=> array ('$in'=>$myPros)));
+			$agrees = $this->db->Proworth->find (
+				array (
+					'like_seed'=>$seed_id,
+					'like_user'=> array ('$in'=>$myPros)
+					)
+				);
 
 			//计算初始温度的当前热度
 			$hotness = $this->calculate(100,$seed['seed_time']);
