7cf3c4f145f390b109364a3a9fb27742d4ed6fcf
Zhu Lun <lonblues@gmail.com>
2015-05-06 08:45:24 +0800

    - Debugging;

--- host/Prouser.php
+++ host/Prouser.php
@@ -136,7 +136,7 @@ class Prouser extends YqBase {
 				$user[$profile_type]['user_title'] = $user_title;
 				$user[$profile_type]['user_weight'] = 1;
 				$this->db->Prouser->save ($user);
-				return 1;
+				return json_encode($user);
 				
 			}catch (Exception $e){
 				return $e;
