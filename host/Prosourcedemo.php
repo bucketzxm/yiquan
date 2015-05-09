c9158a30ecfc434690df1f751c1b0fb5fff2a7c6
Zhu Lun <lonblues@gmail.com>
2015-05-09 10:48:06 +0800

    - Create a demo of Prosource;

---
+++ host/Prosourcedemo.php
@@ -0,0 +1,132 @@
+<?php
+	require_once 'YqBase.php';
+
+	function load_file($url) {
+		$ch = curl_init($url);
+		#Return http response in string
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$xml = simplexml_load_string(curl_exec($ch));
+		return $xml;
+	}
+
+
+			$dbname = 'yiquan';
+			$host = 'localhost';
+			$port = '27017';
+			$user = 'test';
+			$pwd = 'yiquanTodo';
+			 
+			$mongoClient = new MongoClient("mongodb://{$host}:{$port}",array(
+			    		'username'=>$user,
+			    		'password'=>$pwd,
+			    		'db'=>$dbname
+			));
+			$db = $mongoClient->yiquan;
+			$prosource = $db->Prosource;
+			$sources = $prosource->find();
+			
+			$proseed = $db->Proseed;
+			
+		//提出一个月以前的
+		$currentTime = time();
+		$timeMonthAgo = $currentTime - 86400*30;
+		$db->Proseed->remove (array ('seed_time' => array ('$lt' => $timeMonthAgo)));
+
+		foreach ($sources as $key => $value) {
+			echo "<h2>" . $value['source_name'] . "</h2>";
+			$checkTime = $value['check_time'];
+			$feedurl = $value['source_rssURL'];
+			
+	        $feeds = file_get_contents($feedurl);
+	        $feeds = str_replace("<content:encoded>","<contentEncoded>",$feeds);
+	        $feeds = str_replace("</content:encoded>","</contentEncoded>",$feeds);
+	        $rss = simplexml_load_string($feeds,'SimpleXMLElement', LIBXML_NOCDATA);
+
+	        //Calculate average hotness
+	        /*
+	        $seeds = $db->Proseed->find(array ('seed_sourceID' => (string)$value['_id']))->count();
+	        $likes = $db->Proworth->find(array ('like_seedSource' => (string)$value['_id']))->count();
+	        $avgLikes = $likes/$seeds;
+	        $value['average_hotness'] = $avgLikes;
+	        $db->Prosource->save ($value);
+			*/
+
+			//$rss = load_file($feedurl);
+		
+			foreach ($rss->channel->item as $item) {
+				
+				$aaa = new DateTime ();
+				$postTime = $aaa->createFromFormat($value['time_format'],$item->pubDate)->getTimestamp();
+
+				$title = $item->title;
+				$title = str_replace("？", "", $title);
+				$title = str_replace("！", "", $title);
+				$title = str_replace("，", "", $title);
+				$title = str_replace("。", "", $title);
+				$title = str_replace("“", "", $title);
+				$title = str_replace("”", "", $title);
+				$title = str_replace("【", "", $title);
+				$title = str_replace("】", "", $title);
+				$title = str_replace("：", "", $title);
+				$title = str_replace(" ", "", $title);
+				//Split keywords
+				$titleLen = mb_strlen($title,'utf-8');
+				$keywords = array ();
+				for ($i = 0; $i<$titleLen;$i++){
+					$twoStr = mb_substr($title, $i,2,'utf-8');
+					array_push($keywords,$twoStr);
+					$threeStr = mb_substr($title, $i,3,'utf-8');
+					array_push($keywords,$threeStr);
+				}
+
+				//Add code to check whether the word is in the keyword category for specific category
+
+
+				$description = $item->description;
+		        $content = $item->contentEncoded;
+		        $desString = $description;
+		        $contentString = $content;
+		        $desLen = strlen($desString);
+		        $contentLen = strlen($contentString);
+		        $text = '';
+
+		        if ($desLen < $contentLen) {
+		        	$text = $contentString;
+		        }else{
+		        	$text = $desString;
+		        }
+
+				if ($postTime < $checkTime){
+					//echo "<h2>" . "已经刷新过了" . "</h2>";
+				}else{
+
+					$seed = array (
+						'seed_source' => $rss->channel->title,
+						'seed_sourceID' => (string)$value['_id'],
+						'seed_title' => $item->title,
+						'seed_link' => $item->link,
+						'seed_text' => $text,
+						'seed_time' =>$postTime,
+						'seed_keywords' =>$keywords
+					);
+				
+					var_dump($keywords);
+					$res = $proseed->save ($seed);
+					
+
+
+					//$timeStamp = ;
+					echo "<h2>" . $item->title . "</h2>";
+					//echo "<h2>" . $titleLen . "</h2>";
+					//echo "<h2>" . $postTime. "</h2>";
+					//echo "<p>" . $item->description . "</p>";
+				}
+
+			}
+			$value['check_time'] = time();
+			$prosource->save($value);
+		}
+
+
+
+?>
