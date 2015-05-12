2355a2085fd96d9dcf1e334dac5c1110a6e0157b
Zhu Lun <lonblues@gmail.com>
2015-05-12 21:44:08 +0800

    add php operation to get down all user mobiles;

---
+++ host/QuoteuserGet.php
@@ -0,0 +1,43 @@
+<?php
+	require_once 'YqBase.php';
+
+	function load_file($url) {
+		$ch = curl_init($url);
+		#Return http response in string
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$xml = simplexml_load_string(curl_exec($ch));
+		return $xml;
+	}
+
+
+			$dbname = 'yiquan';
+			$host = 'localhost';
+			$port = '27017';
+			$user = 'test';
+			$pwd = 'yiquanTodo';
+			 
+			$mongoClient = new MongoClient("mongodb://{$host}:{$port}",array(
+			    		'username'=>$user,
+			    		'password'=>$pwd,
+			    		'db'=>$dbname
+			));
+			$db = $mongoClient->yiquan;
+			$prosource = $db->Prosource;
+			$sources = $prosource->find();
+			
+			$proseed = $db->Proseed;
+			
+		$users = $this->db->Quoteuser->find();
+
+
+		foreach ($users as $key => $value) {
+			echo "<h2>BEGIN:VCARD</h2>";
+			echo "<h2>VERSION:2.1</h2>";
+			echo "<h2>TEL:".$value['user_mobile']."</h2>";
+			echo "<h2>END:VCARD</h2>";
+			
+		}
+
+
+
+?>
