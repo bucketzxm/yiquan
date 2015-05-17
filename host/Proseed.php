aa252466cf7c8b633895c01d888ca6c55139b8b7
Zhu Lun <lonblues@gmail.com>
2015-05-17 13:24:25 +0800

    - No delete for read articles;

--- host/Proseed.php
+++ host/Proseed.php
@@ -109,9 +109,9 @@ class Proseed extends YqBase {
 			$unreadSeeds = array ();
 			foreach ($seeds as $key => $seed) {
 				$cursor = $this->db->Proread->findOne(array ('seed_id' => $seed,'user_id'=>$user_id,'read_type'=>'0'));
-				if ($cursor == null) {
+				//if ($cursor == null) {
 					array_push($unreadSeeds,$seed);
-				}
+				//}
 			}
 			
 			$res = array ();
