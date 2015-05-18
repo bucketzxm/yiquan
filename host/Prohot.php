ca7ac602c6ef50e63372546e14c6bef13117f4a8
Zhu Lun <lonblues@gmail.com>
2015-05-19 00:15:11 +0800

    - Debugging;

--- host/Prohot.php
+++ host/Prohot.php
@@ -25,7 +25,12 @@
 			$seeds = $db->Proseed->find(array('seed_hotness' => array('$gt' => 1)));
 			foreach ($seeds as $key => $seed) {
 				$para = $db->Prosystem->findOne(array('para_name'=>"user_count"));
-				$seed['seed_hotness'] = $seed['seed_hotness'] * exp(-($para[$seed['seed_industry']]*0.0001) * ((time() - $seed['seed_hotnessTime'])/3600));
+				if ($para[$seed['seed_industry']] == null) {
+					$speed = 0;
+				}else{
+					$speed = $para[$seed['seed_industry']];
+				}
+				$seed['seed_hotness'] = $seed['seed_hotness'] * exp(-($speed*0.0001) * ((time() - $seed['seed_hotnessTime'])/3600));
 				$seed['seed_hotnessTime'] = time();
 				$db->Proseed->save($seed);
 			}
