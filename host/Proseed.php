9d0297bb4e68efa38a051aa13cc189bbbe7d0f15
Zhu Lun <lonblues@gmail.com>
2015-05-19 18:05:10 +0800

    - Debugging;

--- host/Proseed.php
+++ host/Proseed.php
@@ -546,7 +546,7 @@ function queryMySeedsByKeyword($user_id,$time,$keyword){
 			$seed = $this->db->Proseed->find(array ('_id'=> new MongoId($value['like_seed'])));
 
 			foreach ($seed as $key => $item) {
-				if (strpos($item['seed_title'][0],$keyword) < strlen($item['seed_title'])) {
+				if (strpos($item['seed_title'][0],$keyword) < strlen($item['seed_title'][0])) {
 					$item['like_comment'] = $value['like_comment'];
 					//$item['seed_agreeCount'] = $this->db->Proworth->find (array('like_seed' => (string)$item['_id']))->count ();
 					array_push ($myLikedSeeds, $item);	
