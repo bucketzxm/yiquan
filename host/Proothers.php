880b2e133121faf155c78dba10befd7c7e74c1ef
Zhu Lun <lonblues@gmail.com>
2015-05-30 17:51:33 +0800

    - Add a temporary file to deal with databases.

---
+++ host/Proothers.php
@@ -0,0 +1,35 @@
+<?php
+	require_once 'YqBase.php';
+
+	function load_file($url) {
+		$ch = curl_init($url);
+		#Return http response in string
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$xml = simplexml_load_string(curl_exec($ch));
+		return $xml;
+	}
+
+
+			$dbname = 'yiquan';
+			$host = 'localhost';
+			$port = '27017';
+			$user = 'test';
+			$pwd = 'yiquanTodo';
+			 
+			$mongoClient = new MongoClient("mongodb://{$host}:{$port}",array(
+			    		'username'=>$user,
+			    		'password'=>$pwd,
+			    		'db'=>$dbname
+			));
+			$db = $mongoClient->yiquan;
+			$sources = $db->Prosource->find();
+			foreach ($sources as $key => $source) {
+				$source['check_time'] = time() - 1000000000;
+				$db->Prosource->save($source);
+			}
+			
+		
+
+
+
+?>
