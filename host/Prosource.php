825d6ef536812f5a5be2647ea8288005a8c9e712
Delton Ding <dsh0416@gmail.com>
2015-06-02 23:28:42 +0800

    fix syntax error

--- host/Prosource.php
+++ host/Prosource.php
@@ -591,7 +591,7 @@
 									$text = str_replace("height", "", $text);
 									$text = str_replace("font-size", "", $text);
 									$text = str_replace("size=", "", $text);
-									  */  
+									
 
 									$title = $seed['title'];
 									$title = preg_replace("/<.+?>/", "", $title);
