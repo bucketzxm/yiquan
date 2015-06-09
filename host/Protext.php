272a882d58ff12ed5a179853b13dfa3852ab6a12
Zhu Lun <lonblues@gmail.com>
2015-06-09 16:25:10 +0800

    - 将英文单词的匹配条件从等于改为包含；

--- host/Protext.php
+++ host/Protext.php
@@ -165,7 +165,7 @@ function parseTitle($title,$dict){
         preg_match_all("(\\d+.\\d+|\\w+)", $title, $keywords_eng);
 
         foreach ($keywords_eng[0] as $keyy => $valuey) {
-            if ($valuey == $word) {
+            if (strpos($valuey,$word) !== false) {
                 return 1;
             }
         }
