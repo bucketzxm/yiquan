76cd0e591081e96f44294160506f5a99ac3f07b6
Zhu Lun <lonblues@gmail.com>
2015-06-15 15:54:24 +0800

    - Debugging;

--- host/ProImage.php
+++ host/ProImage.php
@@ -42,7 +42,7 @@ foreach ($uncompleteSeeds as $key => $seed) {
     $html = curl_exec($ch);
 
     echo '<h3>'.$feedurl.'</h3>';
-    echo '<h3>'.$html.'</h3>';
+    echo '<h3>'.strlen($html).'</h3>';
 
     //HTML进行UTF-8转码
     $encode = mb_detect_encoding($html, array('ASCII', 'UTF-8', 'GB2312', 'GBK', "EUC-CN", "CP936"));
