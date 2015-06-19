c917f5e008b061b1a518d21362f471d043b36be6
Zhu Lun <lonblues@gmail.com>
2015-06-19 15:48:27 +0800

    - Debugging;

--- host/Prosource.php
+++ host/Prosource.php
@@ -1149,7 +1149,7 @@ foreach ($uncompleteSeeds as $key => $seed) {
                 }
             }
 
-
+            var_dump($seed);
             $db->Proseed->save($seed);
             echo $seed['seed_source'].','.$seed['seed_title'].','.$seed['seed_imageLink'];
     }else{
