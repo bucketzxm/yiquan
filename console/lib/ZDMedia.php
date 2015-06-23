802496d2e63a00a11e08005ec43376e5e80df185
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-23 17:24:41 +0800

    no message

--- console/lib/ZDMedia.php
+++ console/lib/ZDMedia.php
@@ -16,22 +16,24 @@ class Media extends YqBase{
 	function getAllmediaInfo($congifs=[]){
 		$cus=$this->db->Prosource->find();
 			
-		while ( $cus->hasNext () ) {
-			$doc = $cus->getNext ();
-			$this->getMediaDetailInfo ( $doc );
-			$ans [] = $doc;
+		foreach ($cus as $key => $value) {
+			echo $value['source_name'];
+			echo $value['source_description'];
+			echo $value['source_industry'];
+			echo $value['source_rssURL'];
+			echo $value['source_tag'];
+			echo $value['text_openingTag'];
+			echo $value['text_closingTag'];
 		}
-		
-	}
 
-	function getMediaDetailInfo(&$arr){
-		$arr['source_name']=$this->db->Prosource->find(array('source_name'=>$arr['source_name']));
+	
+		/*$arr['source_name']=$this->db->Prosource->find(array('source_name'=>$arr['source_name']));
 		$arr['source_description']=$this->db->Prosource->find(array('source_description'=>$arr['source_description']));
 		$arr['source_industry']=$this->db->Prosource->find(array('source_industry'=>$arr['source_description']));
 		$arr['source_rssURL']=$this->db->Prosource->find(array('source_rssURL'=>$arr['source_rssURL']));
 		$arr['source_tag']=$this->db->Prosource->find(array('source_tag'=>$arr['source_tag']));
 		$arr['text_openingTag']=$this->db->Prosource->find(array('text_openingTag'=>$arr['text_openingTag']));
 		$arr['text_closingTag']=$this->db->Prosource->find(array('text_closingTag'=>$arr['text_closingTag']));
-
-	}
+*/
+	
 }
