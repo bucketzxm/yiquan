8903ed2b9c3599e717d4e87711ae23c55fe7e4cb
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-23 16:03:26 +0800

    no message

--- console/lib/ZDMediaView.php
+++ console/lib/ZDMediaView.php
@@ -6,12 +6,13 @@ require_once 'ZDMedia.php';
 ####################
 
 function combiner($head,$tail){
-	function combine_helper($content){
-	echo "$head"."$content"."tail";
+	$helper= function %(combine_helper) use ($content){
+		echo $head.$content.$tail;
+	return $helper;
 	}
 }
-th_combiner=combiner('<th>','</th>');
-td_combiner=combiner('<td>','</th>');
+$th_combiner=combiner('<th>','</th>');
+$td_combiner=combiner('<td>','</th>');
 #####################################
 
 ###########
@@ -25,24 +26,24 @@ class MediaView extends Media{
 		echo '<div class="table-responsive"><table class="table table-striped">';
 		echo '<thead><tr>';
 
-		th_combiner('媒体名称');
-		th_combiner('媒体描述');
-		th_combiner('媒体所在行业或所属标签';
-		th_combiner('网址或RSS地址');
-		th_combiner( 'sourceTag');
-		th_combiner('openingTag');
-		th_combiner(('closingTag');
+		$th_combiner('媒体名称');
+		$th_combiner('媒体描述');
+		$th_combiner('媒体所在行业或所属标签';
+		$th_combiner('网址或RSS地址');
+		$th_combiner( 'sourceTag');
+		$th_combiner('openingTag');
+		$th_combiner(('closingTag');
 
 
 	for($i = $start; $i < min ( $start + $len, count ( $arr ) ); $i ++) {
 		echo '<tr>';
 		$uid = $arr [$i] ['_id']->{'$id'};
-		td_combiner(is_notempty($arr[$i]['source_name']));
-		td_combiner(is_notempty($arr[$i]['source_description']));
-		td_combiner(is_notempty($arr[$i]['source_rssURL']));
-		td_combiner(is_notempty($arr[$i]['soure_tag']));
-		td_combiner(is_notempty($arr[$i]['text_openingTag']));
-		td_combiner(is_notempty($arr[$i]['tag_closingTag']));
+		$td_combiner(is_notempty($arr[$i]['source_name']));
+		$td_combiner(is_notempty($arr[$i]['source_description']));
+		$td_combiner(is_notempty($arr[$i]['source_rssURL']));
+		$td_combiner(is_notempty($arr[$i]['soure_tag']));
+		$td_combiner(is_notempty($arr[$i]['text_openingTag']));
+		$td_combiner(is_notempty($arr[$i]['tag_closingTag']));
 		echo '</tr>';
 
 
