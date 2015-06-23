a11707fc2fbf49f0ae0d034cf22d9fe7d9175dfa
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-24 00:40:02 +0800

    no message

--- console/lib/ZDMediaView.php
+++ console/lib/ZDMediaView.php
@@ -44,7 +44,7 @@ class MediaView extends Media{
 			td_combiner((isset($arr[$i]['source_description']) ? $arr[$i]['source_description']: ''));
 			td_combiner((isset($arr[$i]['source_industry']) ? implode(',',$arr[$i]['source_industry']): ''));
 			td_combiner((isset($arr[$i]['source_rssURL'])? implode(',',$arr[$i]['source_rssURL']):''));
-			td_combiner("$tag_content");
+			td_combiner(' '."$tag_content".' ');
 			td_combiner((isset($arr[$i]['text_startingTag']) ? $arr[$i]['text_startingTag']: ''));
 			td_combiner((isset($arr[$i]['text_closingTag']) ? $arr[$i]['text_closingTag']: ''));
 			echo '</tr>';
