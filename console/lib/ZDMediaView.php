d3c26a47a7379ad627f244e0c665ac5d42c90b22
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-23 16:47:21 +0800

    no message

--- console/lib/ZDMediaView.php
+++ console/lib/ZDMediaView.php
@@ -5,15 +5,15 @@ require_once 'ZDMedia.php';
 #    combiners     #
 ####################
 
-function combiner($head,$tail){
-	$helper= function ($combine_helper) use ($content){
-		echo $head.$content.$tail;
-	};
-	return $helper;
+
+
+function th_combiner($content){
+	echo '<th>'.$content.'</th>';
 }
-$th_combiner=combiner('<th>','</th>');
-$td_combiner=combiner('<td>','</th>');
-#####################################
+
+function td_combiner($content){
+	echo '<td>'.$content.'</td>';
+}#####################################
 
 ###########
 function is_notempty($name){
@@ -26,24 +26,24 @@ class MediaView extends Media{
 		echo '<div class="table-responsive"><table class="table table-striped">';
 		echo '<thead><tr>';
 
-		$th_combiner('媒体名称');
-		$th_combiner('媒体描述');
-		$th_combiner('媒体所在行业或所属标签');
-		$th_combiner('网址或RSS地址');
-		$th_combiner( 'sourceTag');
-		$th_combiner('openingTag');
-		$th_combiner('closingTag');
+		th_combiner('媒体名称');
+		th_combiner('媒体描述');
+		th_combiner('媒体所在行业或所属标签');
+		th_combiner('网址或RSS地址');
+		th_combiner( 'sourceTag');
+		th_combiner('openingTag');
+		th_combiner('closingTag');
 
 
 		for($i = $start; $i < min ( $start + $len, count ( $arr ) ); $i ++) {
 			echo '<tr>';
 			$uid = $arr [$i] ['_id']->{'$id'};
-			$td_combiner(is_notempty($arr[$i]['source_name']));
-			$td_combiner(is_notempty($arr[$i]['source_description']));
-			$td_combiner(is_notempty($arr[$i]['source_rssURL']));
-			$td_combiner(is_notempty($arr[$i]['soure_tag']));
-			$td_combiner(is_notempty($arr[$i]['text_openingTag']));
-			$td_combiner(is_notempty($arr[$i]['tag_closingTag']));
+			td_combiner(is_notempty($arr[$i]['source_name']));
+			td_combiner(is_notempty($arr[$i]['source_description']));
+			td_combiner(is_notempty($arr[$i]['source_rssURL']));
+			td_combiner(is_notempty($arr[$i]['soure_tag']));
+			td_combiner(is_notempty($arr[$i]['text_openingTag']));
+			td_combiner(is_notempty($arr[$i]['tag_closingTag']));
 			echo '</tr>';
 
 
