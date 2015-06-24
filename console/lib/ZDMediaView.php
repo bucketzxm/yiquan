cd4546e5f699b59b26226d4c2ca0c9514cb39699
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-24 11:17:34 +0800

    no message

--- console/lib/ZDMediaView.php
+++ console/lib/ZDMediaView.php
@@ -68,7 +68,7 @@ class MediaView extends Media{
 		echo '<div class="form-group"><h2>媒体名称</h2>';
 		echo '<textarea class="form-control" rows="3" cols="80" name="name">' . $arr ['source_name'] . '</textarea></div>';
 		echo '<div class="form-group"><h2>媒体描述</h2>';
-		echo '<textarea class="form-control" rows="3" cols="80" name="description">' . (isset($arr ['source_description']) ? $arr ['source_description']:'' . '</textarea></div>';
+		echo '<textarea class="form-control" rows="3" cols="80" name="description">' . (isset($arr ['source_description']) ? $arr ['source_description']:'' ). '</textarea></div>';
 		echo '<div class="form-group"><h2>行业或标签</h2>';
 		echo '<textarea class="form-control" rows="3" cols="80" name="industry">' . (isset($arr ['source_industry']) ? implode(',', $arr ['source_industry']) :''). '</textarea></div>';
 		echo '<div class="form-group"><h2>sourceTag/RSS</h2>';
@@ -76,7 +76,7 @@ class MediaView extends Media{
 		echo '<div class="form-group"><h2>startingTag</h2>';
 		echo '<textarea class="form-control" rows="3" cols="80" name="startingTag">' . (isset($arr ['text_startingTag']) ? $arr ['text_startingTag']:'' ). '</textarea></div>';
 		echo '<div class="form-group"><h2>closingTag</h2>';
-		echo '<textarea class="form-control" rows="3" cols="80" name="closingTag">' . (isset($arr ['text_closingTag'])?$arr ['text_closingTag']:')' . '</textarea></div>';
+		echo '<textarea class="form-control" rows="3" cols="80" name="closingTag">' . (isset($arr ['text_closingTag'])?$arr ['text_closingTag']:'') . '</textarea></div>';
 		echo '<div class="form-group"><h2>状态</h2>';
 		echo '<textarea class="form-control" rows="3" cols="80" name="status">' . (isset($arr ['source_status']) ? $arr ['source_status']:'') . '</textarea></div>';
 		echo '<div class="form-group"><h2>网址或RSS</h2>';
