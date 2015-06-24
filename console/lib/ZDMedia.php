c2560826299138cd7d9e85f8c5c38826ffb23e4a
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-24 17:01:14 +0800

    no message

--- console/lib/ZDMedia.php
+++ console/lib/ZDMedia.php
@@ -72,7 +72,7 @@ class Media extends YqBase{
 			if (isset($row ['text_startingTag']) && isset($arr['startingTag'])) {
 				$row ['text_startingTag'] = $arr ['startingtag'];
 
-			} else if ($arr['startingTag']){
+			} else if (isset($arr['startingTag'])){
 				
 				$row->insert(array( 'text_startingTag' => $arr['startingTag']));
 
