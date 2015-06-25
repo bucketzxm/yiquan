22233a36e9261d7a7c73052dd9a5e78826142b63
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-25 17:21:10 +0800

    no message

--- console/lib/ZDGroupView.php
+++ console/lib/ZDGroupView.php
@@ -130,7 +130,7 @@ class GroupView extends Group {
 		echo '<div class="form-group"><h2>Group名称</h2>';
 		echo '<textarea class="form-control" rows="3" cols="80" name="title">' . $arr ['mediaGroup_title'] . '</textarea></div>';
 		
-		echo '<div class="form-group"><h2>Group媒体,请从输入框中删除媒体,在下方菜单选择新添媒体</h2>';
+		echo '<div class="form-group"><h2>Group媒体<br>请从输入框中删除媒体(最末逗号要清除),在下方菜单选择新添媒体</h2>';
 		if (isset($arr['mediaGroup_sourceList'])) {
 
 
