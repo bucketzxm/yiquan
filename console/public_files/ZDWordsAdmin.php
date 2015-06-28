9597596d7b36ba1636fbd58a240c532efaae5435
Yuzhu Zhang <yuzhuzhang2003@163.com>
2015-06-28 23:23:08 +0800

    no message

--- console/public_files/ZDWordsAdmin.php
+++ console/public_files/ZDWordsAdmin.php
@@ -51,8 +51,9 @@ ob_start ();
 								$a->showOneWords_form ( $a->queryWords( array (
 									'type' => 'findone',
 									'value' => $_GET ['mindex'] 
-							) ) );echo '1';
+							) ) );
 							} else if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
+								echo '1';
 								if ($a->updateWords ( $_POST )) {
 									echo '编辑成功';
 								} else {
