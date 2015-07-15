f0eb60a88f8083d423ecbe6d5faef592c966072e
Zhu Lun <lonblues@gmail.com>
2015-07-15 15:55:00 +0800

    - Debugging;

--- console/lib/ZDMedia.php
+++ console/lib/ZDMedia.php
@@ -34,7 +34,7 @@ class Media extends YqBase{
 	function queryMedia($configs = []) {
 		$ans = [ ];
 		if (empty ( $configs )) {
-			$cus = $this->db->Prosource->find(array('source_domain' => 'business'));
+			$cus = $this->db->Prosource->find(array('source_domain' => 'business','source_status' => 'active'));
 			while ( $cus->hasNext () ) {
 				$doc = $cus->getNext ();
 				$ans [] = $doc;
