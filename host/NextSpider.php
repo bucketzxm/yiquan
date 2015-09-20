cf60f814346a8a3634aa05bb0e5520031652e3aa
Zhu Lun <lonblues@gmail.com>
2015-09-20 18:34:30 +0800

    no message

--- host/NextSpider.php
+++ host/NextSpider.php
@@ -68,7 +68,7 @@ while ($daysCount <= 5) {
                 }
 
 
-                $feeds = preg_replace("/[\t\n\r]+/", "",$feeds);
+                //$feeds = preg_replace("/[\t\n\r]+/", "",$feeds);
                 
                 $pattern = '<a class=\"vote-count\">(.*?)</span>';
                 /*$pattern = "<span class=\"vote-count\">(.*?)</span>.*?<a class=\"post-url.*?>(.*?)</a>.*?<span class=\"post-tagline\">(.*?)</span>";*/
