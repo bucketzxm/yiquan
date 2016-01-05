f31de475633d30b3fcab2f2d379fbbb08c9a5a41
Zhu Lun <zhulun@oneto-tech.com>
2016-01-05 18:05:39 +0800

    no message

--- console/public_files/REAccountAdmin.php
+++ console/public_files/REAccountAdmin.php
@@ -17,7 +17,7 @@ include_once '401.php';
 	<div class="row">
 		<div class="col-sm-3 col-md-2 sidebar">
 			<ul class="nav nav-sidebar">
-				<li><a href="?action=学校信息查询">学校信息查血<span class="sr-only">(current)</span></a></li>
+				<li><a href="?action=学校信息查询">学校信息查询<span class="sr-only">(current)</span></a></li>
 			</ul>
 		</div>
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
